e2205668185b2ea3151b599da7687139fab8f328
rishkumaria <33562078+rishkumaria@users.noreply.github.com>
2018-03-11 22:18:43 -0600

    Add files via upload

---
+++ JavaASTTest.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.CompilationUnit;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+
+
+public class JavaASTTest
+{
+	public static String getFileContent (String filePath) throws FilNotFoundException, IOException
+	{
+		BufferedReader br = new BufferedReader(new FileReader(filePath));
+		StringBuilder sb = new StringBuilder();
+		String line = br.readLine();
+		while (line != null)
+		{
+			sb.append(line);
+			sb.append(System.lineSeparator());
+			line = br.readLine();
+		}
+		return sb.toString();
+	}
+
+
+	public static void main(String[] args)
+	{
+		String filePath = "Point.java";
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
+		char[] fileContent = getFileContent(filePath).toCharArray();
+		parser.setSource(fileContent);
+		CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+
+		cu.accept(new ASTVisitor()
+		{
+			Set names = new HashSet();
+
+			public boolean visit(VariableDeclarationFragment node) 			//change this to type Java.lang.string
+			{
+				SimpleName name = node.getName();
+				int lineNumber = cu.getLineNumber(name.getStartPosition());
+
+				System.out.println("Name: " + name.toString());
+				System.out.println("Line: " + lineNumber);
+				System.out.println("----------------------------");
+				return false;
+
+			}
+		});
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Point.java
@@ -0,0 +1,16 @@
+
+
+
+public class Point
+{
+	private int x;
+	private int y;
+	private int edgeID;
+
+
+	public Point(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+}
